861adc58dc5eddef4d203f50260184afb86a1ccd
Alan Petersen <alanpetersen@mac.com>
2018-04-19 11:06:11 -0700

    fixed lint error

--- force-app/main/default/aura/AccountList/AccountListController.js
+++ force-app/main/default/aura/AccountList/AccountListController.js
@@ -3,9 +3,9 @@
         var action = component.get("c.findAll");
         action.setCallback(this, function(a) {
             component.set("v.accounts", a.getReturnValue());
-            var event = $A.get("e.c:AccountsLoaded");
-            event.setParams({"accounts": a.getReturnValue()});
-            event.fire();
+            var evt = $A.get("e.c:AccountsLoaded");
+            evt.setParams({"accounts": a.getReturnValue()});
+            evt.fire();
         });
     $A.enqueueAction(action);
     }
